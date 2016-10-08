743285f413d1c09607326d0dd0ae615995e4c234
JSpiner <jspiner@naver.com>
2016-10-08 20:00:56 +0000

    add bot

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Response
 from flask import request
 import json
 import requests
-
+import time
 
 app = Flask(__name__)
 
@@ -30,21 +30,56 @@ def fb_post():
         print(jsonObject['entry'][0]['messaging'][0]['message']['text'])
         text = jsonObject['entry'][0]['messaging'][0]['message']['text']
         if '누구' in text:
-            send_msg(userId, '안녕하세요! 대화형 진단봇 StoneFly입니다!')
+            send_msg(userId, '안녕하세요! 대화형 진단봇 Dr. StoneFly입니다!')
+        elif '안녕' in text:
+            send_msg(userId, '좋은 아침입니다!')
         elif '소개' in text:
             send_msg(userId, '/진단 을 입력하고 상태를 입력하면 여러분의 병을 진단해드려요~')
         elif '/진단' in text:
-            send_msg(userId, '안알랴줌')
+            send_msg(userId, '환자분과 비슷한 증상을 검색중입니다.')
             
+            time.sleep(2)
+
             url = "http://hackathon.yangs.party/"
             params = {
                 'content' : text
             }
-            r = requests.post(url, params = prarms)
+            r = requests.post(url, data = params).json()
             print(r)
 
-    except :
-        print('error')
+            a1 = []
+
+            for row1 in r:
+                for row2 in r[row1]:
+                    if row2 not in a1:
+                        a1.append(row2)
+            result = "혹시 이 중에 비슷한 증상이 있으신가요? \n\n"+"\n".join(a1)+"\n\n 이 중 유사한 증상을 '/결과' 명령과 함께 알려주세요 "
+            send_msg(userId, result)
+            print(a1)
+
+        elif '/결과' in text:
+            send_msg(userId, '환자분의 병을 진단하는 중입니다....')
+            time.sleep(2)
+
+            url = "http://hackathon.yangs.party/andSearch"
+            params = {
+                'content' : text[3:]
+            }
+            r = requests.post(url, data = params).json()
+            print(r)
+
+            a1 = []
+
+            for row1 in r:
+                if row1 not in a1:
+                    a1.append(row1)
+            result = "아래 증상일 가능성이 있습니다.\n\n "+"\n".join(a1)+"\n\n 자세한 사항은 아래 링크 혹은 가까운 병원을 방문해주세요\n추천병원 : 서울삼성암병원(1599-3114)"
+            send_msg(userId, result)
+            print(a1)
+            
+
+    except Exception as e:
+        print('error' + str(e))
         return 'er'
     return 'hi'
 
