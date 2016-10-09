2a3498bb729ec2bb3cc43ffa95725c5f634bc769
JSpiner <jspiner@naver.com>
2016-10-09 01:12:15 +0000

    change logic

--- app.py
+++ app.py
@@ -34,7 +34,13 @@ def fb_post():
         elif '안녕' in text:
             send_msg(userId, '좋은 아침입니다!')
         elif '소개' in text:
-            send_msg(userId, '/진단 을 입력하고 상태를 입력하면 여러분의 병을 진단해드려요~')
+            send_msg(userId, '"나지금아파" 를 입력하고 상태를 입력하면 여러분의 병을 진단해드려요~')
+        elif '나지금아파' in text:
+            send_btn1_1(userId, '환자분의 성별을 입력 해 주세요.')
+        elif '성별_' in text:
+            send_btn1_2(userId, '환자분의 나이를 입력 해 주세요.')
+        elif '나이_' in text:
+            send_msg(userId, '/진단 을 입력하고 현재 환자분의 몸 상태를 간단히 설명해 주세요.(ex : 열이 나고 두통이 있습니다.)')
         elif '/진단' in text:
             send_msg(userId, '환자분과 비슷한 증상을 검색중입니다.')
             
@@ -72,10 +78,16 @@ def fb_post():
 
             for row1 in r:
                 if row1 not in a1:
+                    if len(row1) == 3:
+                        row1 = row1 + "(가장높음)";
                     a1.append(row1)
             result = "아래 증상일 가능성이 있습니다.\n\n "+"\n".join(a1)+"\n\n 자세한 사항은 아래 링크 혹은 가까운 병원을 방문해주세요\n추천병원 : 서울삼성암병원(1599-3114)"
             send_msg(userId, result)
             print(a1)
+        elif 'test1' in text:
+            send_btn1(userId, "메세지를 골라봐")
+        elif 'test2' in text:
+            send_btn2(userId, "메세지를 골라봐222")
             
 
     except Exception as e:
@@ -83,6 +95,192 @@ def fb_post():
         return 'er'
     return 'hi'
 
+def send_btn2(userId, msg):
+    print(userId)
+    print(msg)
+    url = "https://graph.facebook.com/v2.6/me/messages"
+    token = "EAAC3LDhBZCtABABsgraEaeMmmmzxZAWFW6dcZCZAXReIwJGBDjWm8WXePyJNsGqIel4NXZCZB9ZCx3LU76cCGzFvFEbY6rLZCvVk43dGHi77v0GiNmYj4g0xqO0ZCCOYl2JWCqZCClIDs1SiPI5NiBPd8M7U4vhaXqeZBW4t5w2MoOiUwZDZD"
+
+    param = {
+        'access_token':token
+    }
+    data = {
+        'recipient':{
+            'id':userId
+        },
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"generic",
+                    "elements":[
+                        {
+                            "title":"Classic White T-Shirt",
+                            "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png",
+                            "subtitle":"Soft white cotton t-shirt is back in style",
+                            "buttons":[
+                                {
+                                    "type":"postback",
+                                    "title":"Bookmark Item",
+                                    "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    ret = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    params={"access_token": token},
+    data = json.dumps(data),
+    headers={'Content-type': 'application/json'})
+    print(ret.text)
+
+def send_btn1_2(userId, msg):
+    print(userId)
+    print(msg)
+    url = "https://graph.facebook.com/v2.6/me/messages"
+    token = "EAAC3LDhBZCtABABsgraEaeMmmmzxZAWFW6dcZCZAXReIwJGBDjWm8WXePyJNsGqIel4NXZCZB9ZCx3LU76cCGzFvFEbY6rLZCvVk43dGHi77v0GiNmYj4g0xqO0ZCCOYl2JWCqZCClIDs1SiPI5NiBPd8M7U4vhaXqeZBW4t5w2MoOiUwZDZD"
+
+    param = {
+        'access_token':token
+    }
+    data = {
+        'recipient':{
+            'id':userId
+        },
+        'message':{
+            'text':msg,
+            "quick_replies":[
+                {
+                    "content_type":"text",
+                    "title":"나이_10대",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                },
+                {
+                    "content_type":"text",
+                    "title":"나이_20대",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                },
+                {
+                    "content_type":"text",
+                    "title":"나이_30대",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                },
+                {
+                    "content_type":"text",
+                    "title":"나이_40대",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                },
+                {
+                    "content_type":"text",
+                    "title":"나이_50대",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                },
+                {
+                    "content_type":"text",
+                    "title":"나이_60대",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+                }
+            ]
+        }
+    }
+    ret = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    params={"access_token": token},
+    data=json.dumps(data),
+    headers={'Content-type': 'application/json'})
+    print(ret.text)
+
+def send_btn1_1(userId, msg):
+    print(userId)
+    print(msg)
+    url = "https://graph.facebook.com/v2.6/me/messages"
+    token = "EAAC3LDhBZCtABABsgraEaeMmmmzxZAWFW6dcZCZAXReIwJGBDjWm8WXePyJNsGqIel4NXZCZB9ZCx3LU76cCGzFvFEbY6rLZCvVk43dGHi77v0GiNmYj4g0xqO0ZCCOYl2JWCqZCClIDs1SiPI5NiBPd8M7U4vhaXqeZBW4t5w2MoOiUwZDZD"
+
+    param = {
+        'access_token':token
+    }
+    data = {
+        'recipient':{
+            'id':userId
+        },
+        'message':{
+            'text':msg,
+            "quick_replies":[
+                {
+                    "content_type":"text",
+                    "title":"성별_남자",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                },
+                {
+                    "content_type":"text",
+                    "title":"성별_여자",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                }
+            ]
+        }
+    }
+    ret = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    params={"access_token": token},
+    data=json.dumps(data),
+    headers={'Content-type': 'application/json'})
+    print(ret.text)
+
+def send_btn1(userId, msg):
+    print(userId)
+    print(msg)
+    url = "https://graph.facebook.com/v2.6/me/messages"
+    token = "EAAC3LDhBZCtABABsgraEaeMmmmzxZAWFW6dcZCZAXReIwJGBDjWm8WXePyJNsGqIel4NXZCZB9ZCx3LU76cCGzFvFEbY6rLZCvVk43dGHi77v0GiNmYj4g0xqO0ZCCOYl2JWCqZCClIDs1SiPI5NiBPd8M7U4vhaXqeZBW4t5w2MoOiUwZDZD"
+
+    param = {
+        'access_token':token
+    }
+    data = {
+        'recipient':{
+            'id':userId
+        },
+        'message':{
+            'text':msg,
+            "quick_replies":[
+                {
+                    "content_type":"text",
+                    "title":"Red",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                },
+                {
+                    "content_type":"text",
+                    "title":"Red1",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                },
+                {
+                    "content_type":"text",
+                    "title":"Red2",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                },
+                {
+                    "content_type":"text",
+                    "title":"Red3",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                },
+                {
+                    "content_type":"text",
+                    "title":"Red4",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                },
+                {
+                    "content_type":"text",
+                    "title":"Green",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+                }
+            ]
+        }
+    }
+    ret = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    params={"access_token": token},
+    data=json.dumps(data),
+    headers={'Content-type': 'application/json'})
+    print(ret.text)
 
 
 def send_msg(userId, msg):
@@ -104,10 +302,7 @@ def send_msg(userId, msg):
     }
     ret = requests.post("https://graph.facebook.com/v2.6/me/messages",
     params={"access_token": token},
-    data=json.dumps({
-      "recipient": {"id": userId},
-      "message": {"text": msg}
-    }),
+    data=json.dumps(data),
     headers={'Content-type': 'application/json'})
     print(ret.text)
 
